fe37280abc08f21e39abdcbc6464978b81872915
Jongwon Woo <jongwonwoo1@gmail.com>
2017-03-21 17:12:13 +0900

    데모앱 레이아웃 추가.

--- Example/JWThumbnailsNavigation/ViewController.swift
+++ Example/JWThumbnailsNavigation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var toolbarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
